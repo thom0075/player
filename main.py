7f07ce9ed33f0f87a15de6123517e4fd9903ca68
thoma <thomascellitti@aol.com>
2022-12-11 17:35:44 +0100

    Created the basic window menu and fixed bugs

--- main.py
+++ main.py
@@ -7,11 +7,29 @@ import sys
 
 class Window(QMainWindow):
     def __init__(self):
-        super().__init__(self)
-        self.setWindowTitle(f"Audio player - Playing {current_track}")  # TODO set variable current_track
-        self.setWindowIcon("qt.png")
-        self.setGeometry(500, 300, 600, 200)
-        self.create_menu()  #calls this method to create the top menu
+        super().__init__()  # calls parent class' constructor
+        self.setWindowTitle(f"Audio player - Playing ")  # TODO set variable current_track {current_track}
+        self.setWindowIcon(QIcon("qt.png"))
+
+        self.setGeometry(500, 300, 400, 300)
+        self.create_menu()  # calls this method to create the top menu
+
+    def create_menu(self):
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+
+        importTracks = QAction("Import tracks", self)  # TODO add icon(s) here
+        importTracks.setShortcut("Ctrl+N")
+
+        exitAction = QAction("Exit", self)
+        exitAction.setShortcut("Alt+F4")
+
+        fileMenu.addAction(importTracks)
+        fileMenu.addAction(exitAction)
+
+    def close_window(self):
+            self.close()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
