c046e09ad14cdee7503333492493686fd98dba6f
thoma <thomascellitti@aol.com>
2022-12-12 21:37:03 +0100

    Modified the function to discover audio files and added a track list. Modified the program's general layout.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.QtGui import QIcon, QFont, QAction
 from pygame import mixer
 from pathlib import Path
 import glob
+import os
 import sys
 
 
@@ -17,6 +18,8 @@ class Window(QMainWindow):
 
         self.setGeometry(500, 300, 550, 400)
         self.create_menu()  # calls this method to create the top menu
+
+        mixer.init()
 
     def create_menu(self):
         mainMenu = self.menuBar()
@@ -48,15 +51,16 @@ class Window(QMainWindow):
 
         self.trackList = QListWidget(self.centralwidget)
         self.trackList.setGeometry(25, 250, 500, 70)
+        self.trackList.clicked.connect(self.track_selected)
 
         self.playButton = QPushButton(self.centralwidget)
         self.playButton.setObjectName("playButton")  # sets obj name
-        self.playButton.setGeometry(150, 170, 70, 50)  # sets position and dimensions: x,y,width, height
+        self.playButton.setGeometry(240, 170, 70, 50)  # sets position and dimensions: x,y,width, height
         self.playButton.setText("Play")  # sets the button's text
 
         self.backButton = QPushButton(self.centralwidget)
         self.backButton.setObjectName("backButton")
-        self.backButton.setGeometry(240, 170, 70, 50)
+        self.backButton.setGeometry(150, 170, 70, 50)
         self.backButton.setText("Previous")
 
         self.forwardButton = QPushButton(self.centralwidget)
@@ -77,9 +81,9 @@ class Window(QMainWindow):
         self.main_v_layout.addLayout(self.buttons_h_layout)
         self.main_v_layout.addWidget(self.trackList)
 
-    def play_pause(self):
-        pass
-        # mixer.music.load("")
+    def play_pause(self, track):
+        mixer.music.load(track)
+        mixer.music.play()
 
     def previous(self):
         pass
@@ -87,9 +91,20 @@ class Window(QMainWindow):
     def next(self):
         pass
 
+    def track_selected(self):
+        try:
+            track = self.trackList.currentItem()
+            track = track.text()
+            self.setWindowTitle(f"Audio player - Playing: {track}")
+            self.play_pause(self.tracksD[f"{track}"])
+            print(self.tracksD[f"{track}"])
+        except Exception as E:
+            with open("report.txt", "w") as f:
+                f.write("Line 105 "+str(E))
+
     def discover_Tracks(self):
         self.folderPath = QFileDialog.getExistingDirectory(self, 'Select Tracks Folder')  # type: str
-        # print(self.folderPath)
+        print(self.folderPath)
         try:
             self.allFiles = glob.glob(f"{self.folderPath}/*.mp3", recursive=False)
             for i in range(len(self.allFiles)):
@@ -111,15 +126,19 @@ class Window(QMainWindow):
 
     def add_to_list(self, tracks):
         try:
+            self.tracksD = {}
             for i in range(len(tracks)):
                 self.trackList.insertItem(0, Path(tracks[i]).name)
+                self.tracksD[Path(tracks[i]).name] = tracks[i]
+            for key, value in self.tracksD.items():
+                print(f"{key} -> {value}")
         except Exception as E:
             with open("report.txt", "w") as f:
                 f.write(str(E))
 
 
 if __name__ == "__main__":
-    mixer.init()
+    # mixer.init()
     app = QApplication(sys.argv)
     window = Window()
     window.create_ui(window)
